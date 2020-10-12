a7247c5b3c56d1638fa857eaef870017605dacac
N sai Pavan <saipavan.narasaraj@gmail.com>
2020-10-12 02:15:13 -0700

    removes inro text

--- photo-gallery-app/src/Components/app.js
+++ photo-gallery-app/src/Components/app.js
@@ -19,12 +19,9 @@ class App extends React.Component {
         <div className="container py-3">
           <div className="row">
             <div className="col-md-12">
-              <p align="justify">quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p align="justify">
+                {/* INTRO TEXT HERE */}
+              </p>
 
             </div>
           </div>
